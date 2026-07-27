9f9e40e1f138d2ff854dfc73afde321d7285e1f2
Prashna Raut <prashnaraut32@gmail.com>
2026-07-27 17:56:37 +0545

    third commit

--- controller/authController.js
+++ controller/authController.js
@@ -11,11 +11,51 @@ export const register = async (req, res) => {
         return res.json({ success: false, message: 'Missing Details' })
     }
 
-    try { const existingUser = await userModel.findOne({email})
+    try {
+        const existingUser = await userModel.findOne({ email })
 
-    const hashedPassword = await bcrypt.hash(password,10);
+        if (existingUser) {
+            return res.json({ success: false, message: "User already exist" });
+
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new userModel({ name, email, password: hashedPassword });
+        await user.save();
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET,{ expiresIn: '7d'});
+
+        res.cookie('token',token,{
+            httpOnly: true ,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite : process.env.NODE_ENV === 'production' ?
+            'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 *1000
+
+        });
 
     } catch (error) {
         res.json({ success: false, message: error.message })
+    }
+}
+
+export const login = async(req, res)=>{
+    const{email,password} = req.body;
+
+    if(!email || !password){
+        return res.json({success:false,message:'Email and password are required '})
+    }
+    try{
+        const user = await userModel.findOne({email});
+
+        if(!user){
+            return res.json({success:false, message: 'Invalid email'})
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password)
+
+    } catch (error){
+        return res.json({success:false, message: error.message});
     }
 }
