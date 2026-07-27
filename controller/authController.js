d54e808f54f51723b7bd2d60d12770624f4de3ec
Prashna Raut <prashnaraut32@gmail.com>
2026-07-27 21:13:51 +0545

    forth commit

--- controller/authController.js
+++ controller/authController.js
@@ -53,9 +53,32 @@ export const login = async(req, res)=>{
             return res.json({success:false, message: 'Invalid email'})
         }
 
-        const isMatch = await bcrypt.compare(password,user.password)
+        const isMatch = await bcrypt.compare(password,user.password);
+
+        if(!isMatch){
+           return res.json({success:false, message:'Invalid password'}) 
+        }
+
+
+         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET,{ expiresIn: '7d'});
+
+        res.cookie('token',token,{
+            httpOnly: true ,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite : process.env.NODE_ENV === 'production' ?
+            'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 *1000
+
+        });
+
+
+
+        return res.json({success:true});
 
     } catch (error){
         return res.json({success:false, message: error.message});
+    }
+    export const logout = aaync(req,res)=>{
+        
     }
 }
