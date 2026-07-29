191f48f527ef01a586b92faa0d2432f15785f235
Prashna Raut <prashnaraut32@gmail.com>
2026-07-29 10:45:29 +0545

    12th commit

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,7 +1,11 @@
 import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
-
+    host:'smtp-relay.brevo.com'
+    port:587
+    auth:{
+        user:
+    }
 });
 
 export default transporter;
